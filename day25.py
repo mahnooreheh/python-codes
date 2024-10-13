cce3e0c7033c7bc1e443f52ae4c677b4e5007499
mahnooreheh <166315612+mahnooreheh@users.noreply.github.com>
2024-10-14 00:49:42 +0500

    Add files via upload

---
+++ day25.py
@@ -0,0 +1,8 @@
+file = open('weatherdata.csv','r+')
+# for each in file:
+#     print(each)
+#OR
+# print(file.read())
+# print(file.read(10)) #prints the 10 charcters of the file
+# file.write("Hi i wrote this!")
+# print(file.read())
